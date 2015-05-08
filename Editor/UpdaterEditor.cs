d5751fc8a5f13d8e9448679812a1f53e0d43770e
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-08 19:27:16 +0200

    Initial commit

---
+++ Editor/UpdaterEditor.cs
@@ -0,0 +1,59 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace RendererUpdate {
+
+    [CustomEditor(typeof(Updater))]
+    public sealed class UpdaterEditor : Editor {
+        #region FIELDS
+
+        private Updater Script { get; set; }
+
+        #endregion FIELDS
+
+        #region SERIALIZED PROPERTIES
+
+        #endregion SERIALIZED PROPERTIES
+
+        #region UNITY MESSAGES
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            DrawVersionLabel();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void OnEnable() {
+            Script = (Updater)target;
+
+        }
+
+        #endregion UNITY MESSAGES
+
+        #region INSPECTOR
+
+        private void DrawVersionLabel() {
+            EditorGUILayout.LabelField(
+                string.Format(
+                    "{0} ({1})",
+                    Updater.VERSION,
+                    Updater.EXTENSION));
+        }
+
+        #endregion INSPECTOR
+
+        #region METHODS
+
+        [MenuItem("Component/MyNamespace/Updater")]
+        private static void AddUpdaterComponent() {
+            if (Selection.activeGameObject != null) {
+                Selection.activeGameObject.AddComponent(typeof(Updater));
+            }
+        }
+
+        #endregion METHODS
+    }
+
+}
