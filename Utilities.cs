2c6b5383e8d0bcb5e0c4d5b70532f2f701b95a6e
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-09 18:15:38 +0200

    Create Utilities class

---
+++ Utilities.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace RendererUpdate {
+
+    public static class Utilities {
+
+        public static void SetupMaterialWithBlendMode(Material material, BlendMode blendMode) {
+            switch (blendMode) {
+                case BlendMode.Opaque:
+                    material.SetInt("_SrcBlend", 1);
+                    material.SetInt("_DstBlend", 0);
+                    material.SetInt("_ZWrite", 1);
+                    material.DisableKeyword("_ALPHATEST_ON");
+                    material.DisableKeyword("_ALPHABLEND_ON");
+                    material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                    material.renderQueue = -1;
+                    break;
+                case BlendMode.Cutout:
+                    material.SetInt("_SrcBlend", 1);
+                    material.SetInt("_DstBlend", 0);
+                    material.SetInt("_ZWrite", 1);
+                    material.EnableKeyword("_ALPHATEST_ON");
+                    material.DisableKeyword("_ALPHABLEND_ON");
+                    material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                    material.renderQueue = 2450;
+                    break;
+                case BlendMode.Fade:
+                    material.SetInt("_SrcBlend", 5);
+                    material.SetInt("_DstBlend", 10);
+                    material.SetInt("_ZWrite", 0);
+                    material.DisableKeyword("_ALPHATEST_ON");
+                    material.EnableKeyword("_ALPHABLEND_ON");
+                    material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                    material.renderQueue = 3000;
+                    break;
+                case BlendMode.Transparent:
+                    material.SetInt("_SrcBlend", 1);
+                    material.SetInt("_DstBlend", 10);
+                    material.SetInt("_ZWrite", 0);
+                    material.DisableKeyword("_ALPHATEST_ON");
+                    material.DisableKeyword("_ALPHABLEND_ON");
+                    material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                    material.renderQueue = 3000;
+                    break;
+            }
+        } 
+
+    }
+
+}
